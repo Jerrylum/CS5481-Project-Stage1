6b858bfc77a1583284002248ddc349de1a62594d
Jerrylum <serverofjerry@gmail.com>
2024-10-16 19:24:05 +0800

    Remove comments

--- main.py
+++ main.py
@@ -106,26 +106,6 @@ def analyze_page():
 read_analyzed_post_ids()
 
 j = 0
-# try:
-#     while True:
-#         driver = webdriver.Remote(
-#             command_executor="http://host.docker.internal:4444/wd/hub",
-#             options=firefox_options,
-#         )
-#         # use local firefox
-#         # driver = webdriver.Firefox(options=firefox_options)
-
-#         driver.get("https://9gag.com/interest/memes")
-#         analyze_page()
-#         driver.quit()
-#         j += 1
-#         print(f"Reload {j}, sleep 30 minutes")
-#         # sleep 30 minutes
-#         time.sleep(30 * 60)
-# except KeyboardInterrupt:
-#     pass
-# finally:
-#     driver.quit()
 while True:
     driver = webdriver.Remote(
         command_executor="http://host.docker.internal:4444/wd/hub",
